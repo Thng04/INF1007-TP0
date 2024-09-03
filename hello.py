b66103b2574b64798d78cd7c8eb60a8ab630b847
Thng04 <tomnguyen6688@gmail.com>
2024-09-03 16:14:32 -0400

    Affichage de l'aire et du nom

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+#Hoang Thong NGUYEN 2369935
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+aire = math.pi * (rayon**2)  
+print(aire)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("NGUYEN Hoang Thong")
